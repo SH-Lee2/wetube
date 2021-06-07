4d7371c2ad14187bc5d305682271698fd99fa7e5
SH-Lee2 <59095793+SH-Lee2@users.noreply.github.com>
2021-06-08 04:11:34 +0900

    Update videoRouter.js

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,7 +1,7 @@
 import express from "express"
 import {deleteVideo,  getEdit, getUpload, postEdit, postUpload, watch} from "../controllers/videoController"
 const videoRouter = express.Router()
-
+// mongo 의 id 는 24자리 0-9, a-f로 이루어 져있음
 videoRouter.get('/:id([0-9a-f]{24})',watch)
 videoRouter.route('/:id([0-9a-f]{24})/edit').get(getEdit).post(postEdit)
 videoRouter.get('/:id([0-9a-f]{24})/delete',deleteVideo)
